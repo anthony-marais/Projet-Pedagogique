60d11b5ff6b1d8689635d238ac6403fdfb988e4e
anthony-marais <marais.anthony.pro@gmail.com>
2020-11-27 15:28:43 +0100

    update clean DB reservation ## correction erreur

--- Base de donnée/db_reservation.sql
+++ Base de donnée/db_reservation.sql
@@ -25,14 +25,14 @@ DROP TABLE IF EXISTS `INFORMATION`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `INFORMATION` (
-  `in_id` int NOT NULL,
-  `in_mail` varchar(300) DEFAULT NULL,
-  `in_nom` varchar(30) DEFAULT NULL,
-  `in_prenom` varchar(30) DEFAULT NULL,
-  `in_poste` varchar(50) DEFAULT NULL,
-  `in_organisation` varchar(100) DEFAULT NULL,
+  `in_id` int NOT NULL AUTO_INCREMENT,
+  `in_mail` varchar(300) NOT NULL,
+  `in_nom` varchar(30) NOT NULL,
+  `in_prenom` varchar(30) NOT NULL,
+  `in_poste` varchar(50) NOT NULL,
+  `in_organisation` varchar(100) NOT NULL,
   PRIMARY KEY (`in_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ DROP TABLE IF EXISTS `MAIL`;
 CREATE TABLE `MAIL` (
   `ma_id` int NOT NULL AUTO_INCREMENT,
   `ma_contenu` varchar(320) NOT NULL,
-  `in_id` int DEFAULT NULL,
+  `in_id` int NOT NULL,
   PRIMARY KEY (`ma_id`),
   KEY `fk_MAIL_1_idx` (`in_id`),
   CONSTRAINT `fk_MAIL_1` FOREIGN KEY (`in_id`) REFERENCES `INFORMATION` (`in_id`)
@@ -65,8 +65,8 @@ CREATE TABLE `RESERVATION` (
   `res_heure_arrive` int NOT NULL,
   `res_heure_depart` int NOT NULL,
   `res_temps_reserver` int NOT NULL,
-  `sa_id` int DEFAULT NULL,
-  `ma_id` int DEFAULT NULL,
+  `sa_id` int NOT NULL,
+  `ma_id` int NOT NULL,
   PRIMARY KEY (`res_id`),
   KEY `fk_RESERVATION_1_idx` (`ma_id`),
   KEY `fk_RESERVATION_2_idx` (`sa_id`),
@@ -98,4 +98,4 @@ CREATE TABLE `SALLE` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-26 13:48:47
+-- Dump completed on 2020-11-27 15:28:15
