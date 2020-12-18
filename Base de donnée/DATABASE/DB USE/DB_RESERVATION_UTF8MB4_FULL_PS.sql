55c045eda9b1a78d8bcf598a8de1cf37d1b4be9e
Clave <lukas.clave@gmail.com>
2020-12-18 16:40:20 +0100

    Update PL_SALLE en PFind_SALLE

--- Base de donnée/DATABASE/DB USE/DB_RESERVATION_UTF8MB4_FULL_PS.sql
+++ Base de donnée/DATABASE/DB USE/DB_RESERVATION_UTF8MB4_FULL_PS.sql
@@ -700,7 +700,7 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `PL_SALLE` */;
+/*!50003 DROP PROCEDURE IF EXISTS `PFind_SALLE` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -710,7 +710,7 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `PL_SALLE`()
+CREATE DEFINER=`root`@`localhost` PROCEDURE `PFind_SALLE`()
 SELECT sa_id, sa_name FROM SALLE ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
